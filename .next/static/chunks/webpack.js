6f9d7b03b1a7fbceb004b02b98be2a09effc4d7e
MohanManirul <fiforeg@gmail.com>
2023-10-01 15:23:08 +0600

    stats list api calling ok

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "62d0a4d419647661"; }
+/******/ 		__webpack_require__.h = function() { return "dd26aa0921993c36"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
