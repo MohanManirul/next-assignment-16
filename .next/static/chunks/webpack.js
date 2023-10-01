d1cfd3c82d6240a58322f7b09cc3a52a76f47601
MohanManirul <fiforeg@gmail.com>
2023-10-01 23:23:25 +0600

    All servicess  section api calling ok

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "832022ca7b0fe9bd"; }
+/******/ 		__webpack_require__.h = function() { return "1eef080990baea42"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
