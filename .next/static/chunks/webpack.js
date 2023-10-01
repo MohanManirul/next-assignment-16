2302c4b40908215545aaac600dfa3b005bc258ee
MohanManirul <fiforeg@gmail.com>
2023-10-01 14:09:57 +0600

    brand list api calling ok

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "604f62540a8ff5ec"; }
+/******/ 		__webpack_require__.h = function() { return "62d0a4d419647661"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -9,56 +9,32 @@ async function getData(){
 
 const Brand = async () => {
   const data = await getData();
-
   return (
     <>
       <section className="py-20">
         <div className="container mx-auto px-4">
+            <p>Brand List</p>
           <div className="flex flex-wrap -mx-2">
-            <div className="mb-4 w-full md:w-1/2 lg:w-1/4 px-2">
-              <div className="py-16 bg-gray-50 rounded">
-                <a href="#">
-                  <img
-                    className="mx-auto h-8"
-                    src="atis-assets/logo/brands/slack.png"
-                    alt=""
-                  />
-                </a>
-              </div>
-            </div>
-            <div className="mb-4 w-full md:w-1/2 lg:w-1/4 px-2">
-              <div className="py-16 bg-gray-50 rounded">
-                <a href="#">
-                  <img
-                    className="mx-auto h-8"
-                    src="atis-assets/logo/brands/dropbox.png"
-                    alt=""
-                  />
-                </a>
-              </div>
-            </div>
-            <div className="mb-4 w-full md:w-1/2 lg:w-1/4 px-2">
-              <div className="py-16 bg-gray-50 rounded">
-                <a href="#">
-                  <img
-                    className="mx-auto h-8"
-                    src="atis-assets/logo/brands/spotify.png"
-                    alt=""
-                  />
-                </a>
-              </div>
-            </div>
-            <div className="w-full md:w-1/2 lg:w-1/4 px-2">
-              <div className="py-16 bg-gray-50 rounded">
-                <a href="#">
-                  <img
-                    className="mx-auto h-8"
-                    src="atis-assets/logo/brands/stripe.png"
-                    alt=""
-                  />
-                </a>
-              </div>
-            </div>
+              {
+                data.map((item,i)=>{
+                  return(
+                      <div key={i} className="mb-4 w-full md:w-1/2 lg:w-1/4 px-2">
+
+                            <div  className="py-16 bg-gray-50 rounded">
+                              <a href="#">
+                                <img
+                                  className="mx-auto h-8"
+                                  src={item['image']}
+                                  alt=""
+                                />
+                              </a>
+                            </div>
+                      </div>
+                  )
+
+                })
+              }
+        
           </div>
         </div>
       </section>
